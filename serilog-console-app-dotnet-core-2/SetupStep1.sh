eb22f3f39de51283c6c77b95c812b480e826b733
Matthew Erbs <matthew.erbs@merbla.com>
2018-06-07 18:57:20 +1000

    Leftovers

--- serilog-console-app-dotnet-core-2/SetupStep1.sh
+++ serilog-console-app-dotnet-core-2/SetupStep1.sh
@@ -3,11 +3,6 @@
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 lsb_release -a
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-trusty-prod trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-2.0.0
-
-
-echo "------------------------------"
-echo "All Done ;) let's get started."
-echo "------------------------------"
